b3d82efdb4d6392cecad688267bebba693334bc9
ArshjotSaini <ashu73767@gmail.com>
2024-08-06 13:25:40 -0400

    test commit

--- src/main/java/com/StudentsManagementSystem/StudentsManagementSystemApplication.java
+++ src/main/java/com/StudentsManagementSystem/StudentsManagementSystemApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StudentsManagementSystemApplication {
 	public static void main(String[] args) {
+		// main method
 		SpringApplication.run(StudentsManagementSystemApplication.class, args);
 	}
 }
